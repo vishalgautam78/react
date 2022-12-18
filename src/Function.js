fa001759f460068a25261be0711621205b4b076a
vishalgautam78 <vishalgautam78@gmail.com>
2022-12-18 20:01:01 +0530

    edited function file

--- src/Function.js
+++ src/Function.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 function Function() {
     return (
-        <h1>This is function component.</h1>
+        <h1>This is function component and vishal is doing this.</h1>
     )
 }
 
