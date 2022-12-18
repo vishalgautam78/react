a54a6d77431591fb1fbba6ea749901792777134c
vishalgautam78 <vishalgautam78@gmail.com>
2022-12-18 15:19:10 +0530

    Testing git

--- src/Function.js
+++ src/Function.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 function Function() {
     return (
-        <h1>This is function component.</h1>
+        <h1>This is function component and to check the git.</h1>
     )
 }
 
